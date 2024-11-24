c5ae03ff588874587f60a8069adb723e66f1eaaa
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-11-24 17:07:45 +0300

    feat: added mock folders in modalWindow

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <modal-window  />
+  <modal-window :folders="mockFolders" />
   <main>
     <button-component @click="openModal()">Открыть</button-component>
   </main>
@@ -9,6 +9,7 @@
 import ButtonComponent from "@/components/ui/ButtonComponent.vue";
 import ModalWindow from "@/components/ui/modals/ModalWindow.vue";
 import { useModalStore } from "@/stores/modals/modals.ts";
+import type { Folder } from "@/types/Folders.ts";
 
 const modalStore = useModalStore();
 
@@ -16,7 +17,21 @@ const openModal = () => {
   modalStore.openModal();
 };
 
-
+const mockFolders: Folder[] = [
+  {
+    id: 1,
+    name: "Папка 1",
+    children: [
+      { id: 2, name: "Папка 1.1", children: [] },
+      {
+        id: 3,
+        name: "Папка 1.2",
+        children: [{ id: 4, name: "Папка 1.2.1", children: [] }],
+      },
+    ],
+  },
+  { id: 5, name: "Папка 2", children: [] },
+];
 </script>
 
 <style scoped></style>
--- src/components/ui/modals/ModalWindow.vue
+++ src/components/ui/modals/ModalWindow.vue
@@ -1,12 +1,24 @@
 <template>
   <div v-if="true" class="modal-layout" @click.self="modalClose">
-    <div class="modal">modalwindow</div>
+    <div class="modal">
+      <div v-for="(folder, index) in folders" :key="folder.id" class="folder">
+        {{ folder.name }}
+        <div v-if="folder.children.length">
+          <div v-for="(child, index) in folder.children" :key="child.id" class="children">
+            {{ child.name }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, defineProps } from "vue";
 import { useModalStore } from "@/stores/modals/modals.ts";
+import type { Folder } from "@/types/Folders.ts";
+
+defineProps<{ folders: Folder[] }>();
 
 const modalStore = useModalStore();
 
@@ -41,5 +53,11 @@ const modalClose = () => {
   background: #16171d;
   color: white;
   padding: 20px;
+}
+.folder {
+  margin-left: 20px;
+}
+.children {
+  margin-left: 20px;
 }
 </style>
